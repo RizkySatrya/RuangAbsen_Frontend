3b2ee87c6c413d535202488668884aeb38b5627d
Rizky Satrya <rizkysatrya@outlook.co.id>
2022-06-14 15:36:50 +0700

    membuat API google maps

---
+++ app/src/main/java/com/example/ruangabsen/ForgotPasswordActivity.java
@@ -0,0 +1,6 @@
+package com.example.ruangabsen;
+
+import android.app.Activity;
+
+public class ForgotPasswordActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/ruangabsen/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.ruangabsen;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
